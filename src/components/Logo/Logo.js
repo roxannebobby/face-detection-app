4e0a9b0ff87ac1ac78b4d5b541bcb0f903850c8d
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-08 15:18:42 -0500

    logo and nav added

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import face from "./face.png";
+import "./Logo.css";
+
+const Logo = () => {
+	return (
+		<div className="ma4 mt0">
+			<p>
+				<Tilt
+					className="Tilt br2 shadow-2"
+					options={{ max: 55 }}
+					style={{ height: 150, width: 150 }}
+				>
+					<div className="Tilt-inner">
+						<img src={face} />
+					</div>
+				</Tilt>
+			</p>
+		</div>
+	);
+};
+
+export default Logo;
