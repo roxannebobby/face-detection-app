29b78f7018eb0a870de5f7e4aa030095c33e2f7b
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-08 16:18:15 -0500

    before changing create-react-app version

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,12 +5,12 @@ import "./Logo.css";
 
 const Logo = () => {
 	return (
-		<div className="ma4 mt0">
+		<div className="ma4 mt0" style={{ marginTop: "-100px" }}>
 			<p>
 				<Tilt
 					className="Tilt br2 shadow-2"
 					options={{ max: 55 }}
-					style={{ height: 150, width: 150 }}
+					style={{ height: 125, width: 125 }}
 				>
 					<div className="Tilt-inner pa3">
 						<img style={{ paddingTop: "5px" }} src={face} alt="logo" />
