fd575f8f24ca551914b9de991e46c1d419596b78
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-09 15:38:18 -0500

    onSubmitSignIn fixed on Register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -42,7 +42,6 @@ class Register extends React.Component {
 	};
 
 	render() {
-		const { onRouteChange } = this.props;
 		return (
 			<article className="br3 ba b--black-10 mv4 shadow-5 w-100 w-50-m w-25-l mw5 center">
 				<main className="pa4 black-80">
@@ -89,7 +88,7 @@ class Register extends React.Component {
 								className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
 								type="submit"
 								value="Register"
-								onClick={() => onRouteChange("home")}
+								onClick={this.onSubmitSignin}
 							/>
 						</div>
 					</div>
