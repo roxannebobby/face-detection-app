f2b31b52338ef9f140d645fd24cbd05dddc5f5a2
Roxanne Weber <89050736+roxanneweber@users.noreply.github.com>
2022-06-09 15:04:19 -0500

    a few minor fixes to register and signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -76,7 +76,7 @@ class Signin extends React.Component {
 						</div>
 						<div className="lh-copy mt3">
 							<p
-								onClick={() => onRouteChange("register")}
+								onClick={this.onSubmitSignIn}
 								className="f6 link dim black db pointer"
 							>
 								Register
